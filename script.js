31fdd5cc4484d763d319202e15722090151d9d7b
jenish <114239379+jenish2060@users.noreply.github.com>
2024-01-19 20:06:16 +0545

    Get computer choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber){
+        case 0:
+            return 'ROCK';
+        case 1:
+            return 'PAPER';
+        case 2:
+            return 'SCISSORS';
+    }
+}
+
+console.log(getComputerChoice());
