e48bf17132101ef9335ea79bf668dcb1be17d3db
jenish <114239379+jenish2060@users.noreply.github.com>
2024-01-20 09:48:14 +0545

    Add function to play one round

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3);
-    switch(randomNumber){
-        case 0:
-            return 'ROCK';
-        case 1:
-            return 'PAPER';
-        case 2:
-            return 'SCISSORS';
-    }
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
 }
 
 const getPlayerChoice = () => {
-    let playerChoice = prompt("ROCK | PAPER | SCISSORS").toUpperCase();
-    return playerChoice;
+  let playerChoice = prompt("ROCK | PAPER | SCISSORS").toUpperCase();
+  return playerChoice;
 };
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `It's a Tie! ${playerSelection} ties with ${computerSelection}`;
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "ROCK")
+  ) {
+    return `You Lost! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You Won! ${playerSelection} beats ${computerSelection}`;
+  }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
